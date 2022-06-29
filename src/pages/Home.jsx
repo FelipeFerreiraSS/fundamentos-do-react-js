b547506c34195e69b6e4a5284dbaa6c6f41baf33
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-06-28 23:38:37 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,16 @@ function Home() {
   return (
     <main className="flex min-h-screen justify-center overflow-hidden bg-gray-800 py-6 sm:py-12 ">
       <div className="container mx-auto rounded flex flex-col items-center bg-gray-800">
-        <h1 className="text-5xl text-cyan-50 mt-6 mb-6">
+        <h1 className="sm:text-5xl text-4xl text-cyan-50 mt-6 mb-6">
           Lista de Presença
         </h1>
         <input 
-          className="w-1/2 border-none rounded mt-6 mb-6 py-3 px-2 text-xl focus:outline-none"
+          className="w-3/4 sm:w-2/3 lg:w-1/2 border-none rounded mt-6 mb-6 py-3 px-2 text-xl focus:outline-none"
           type="text" 
           placeholder="Digite o nome..."
           />
         <button 
-          className="bg-blue-600 w-1/2 py-3 text-xl font-bold text-cyan-50 mb-16 rounded"
+          className="bg-blue-600 w-3/4 sm:w-2/3 lg:w-1/2 py-3 text-xl font-bold text-cyan-50 mb-16 rounded"
           type="button"
         > 
           Adicionar
